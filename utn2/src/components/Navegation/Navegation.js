2e287ce03678abd453507cad49a1c02e713cc18e
RS <112523550+robertoschwarz78@users.noreply.github.com>
2023-01-05 17:29:18 -0300

    subida parcial

---
+++ utn2/src/components/Navegation/Navegation.js
@@ -0,0 +1,24 @@
+import {Link} from "react-router-dom";
+import './Navegation.css';
+
+export default function Navegation() {
+    return(
+       <header className="d-flex bg-secondary">
+            <h1>Rick & Morty </h1>
+            <nav>
+                <ul className="d-flex">
+                    <li className="mx-2"> 
+                        <Link to="/">Home</Link>  
+                    </li>
+                    <li className="mx-2">
+                        <Link to="/characters">Characters</Link>
+                    </li>
+                    <li className="mx-2">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+       </header>
+    )
+}
+

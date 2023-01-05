0c703aa104027b5c2ef08a86d81a86e357f32ad8
RS <112523550+robertoschwarz78@users.noreply.github.com>
2023-01-05 18:52:08 -0300

    subida parcial

--- utn2/src/components/Home/Home.js
+++ utn2/src/components/Home/Home.js
@@ -1,17 +1,21 @@
+import "./Home.css";
+import ListItemHome from "../ListItemHome/ListItemHome";
+
 export default function Home() {
 
     return(
         
         <header className="bg-primary">
-            <h1 >Proyecto - Rick & Morty</h1>
+            <h1 >Proyect - Rick & Morty</h1>
             <nav className="navbar">
                 <ul className="d-flex justify-content-between">
 
-                    Item 1
-                    Item 2
+                    <ListItemHome dato="Characters" infoPath="/characters"/>
+                    <ListItemHome dato="Contact" infoPath="/contact"/>
                     
                 </ul>
             </nav>
         </header>
     )
 }
+
---
+++ utn2/src/components/SectionCards/SectionCards.js
@@ -0,0 +1,17 @@
+import Card from "../Card/Card";
+import './SectionCards.css'
+
+export default function SectionCards({datos}) {
+    return(
+        <section className="section-cards">
+
+            {datos.map((dato)=>{ 
+                return <Card key={dato.id} info={dato}/>
+            })} 
+         
+        </section>
+    )
+}
+
+
+/* fetch()  */
